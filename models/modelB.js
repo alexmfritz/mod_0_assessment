03d36a7bc27af132e409d6a9854212fb146a2bf5
Alex Fritz <alexfritzcoach@gmail.comm>
2021-09-22 12:17:01 -0700

    Create object model for developer

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  company: "Valve Software",
+  jobTitle: "Steam Software Engineer - Lead",
+  salary: 115250,
+  ptoWeeks: 6,
+  isFullStack: true,
+  hasHealthInsurance: true,
+  languages: ["C++", "C#", "PHP", "JavaScript", "Ruby", "Python", "jQuery"],
+  qualifications: ["Bachelor's Degree in Computer Science", "Bachelor's Degree in Computer Engineering", "Master of Science in Computer Science"]
+};
