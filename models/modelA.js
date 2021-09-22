7e9f04fd32d7ad7d6f28bcf004a0978ddd7cbe1e
Alex Fritz <alexfritzcoach@gmail.comm>
2021-09-22 12:12:51 -0700

    Create object model for dinosaur

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  genus: "Ankylosaurus",
+  eraLived: "late Cretaceous period",
+  maxHeightFeet: 26,
+  maxWeightTons: 8,
+  canFly: false,
+  eatsMeat: false,
+  diet: ["tough leaves", "pulpy fruit", "low-browse crop"],
+  funFacts: ["its name means 'fused lizard'", "had large clubbed tails to fight off T.rexes", "belongs to a unique group of heavily armored dinosaurs"]
+};
