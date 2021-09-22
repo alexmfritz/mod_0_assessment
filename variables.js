e4c230382de01902095b2808bdb0040c553913c5
Alex Fritz <alexfritzcoach@gmail.comm>
2021-09-22 12:07:24 -0700

    Complete program specific challenges

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = "Good Will Hunting";
+var isEntertaining = true;
+movie = "Shang-Chi and the Legend of the Ten Rings";
+var num4 = 4;
+var num6 = 6;
+var product = num6 * num4;
+var favoriteFoods = ["pepperoni pizza", "phad see ew", "stir fry"];
+favoriteFoods.shift();
+console.log(movie.length);
